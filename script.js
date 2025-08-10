c3c0e97703633de553e540133d14571d5e03df22
lcsbv <lucas03bezerra@gmail.com>
2025-08-10 19:15:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function learnMore() {
+  // Código do Brasil é 55, então o número fica 5562998052045
+  window.open('https://wa.me/5562998052045?text=Olá,%20me%20informe%20mais%20sobre%20este%20robô', '_blank');
+}
